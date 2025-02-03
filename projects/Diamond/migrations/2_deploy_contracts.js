702ae52dd057123b308ddc8099727b154a423856
adeelch9 <adeel.chaudhary@tron.network>
2025-02-03 08:46:36 -0500

    code improvements

--- projects/Diamond/migrations/2_deploy_contracts.js
+++ projects/Diamond/migrations/2_deploy_contracts.js
@@ -5,8 +5,8 @@ const DiamondCutFacet = artifacts.require('DiamondCutFacet');
 const DiamondInit = artifacts.require('DiamondInit');
 const Test1Facet = artifacts.require('Test1Facet');
 const Test2Facet = artifacts.require('Test2Facet');
-const tronWeb = require('tronweb');
-const crypto = require('crypto');
+const { TronWeb } = require('tronweb');
+const { ethers } = require('ethers');
 
 const FacetNames = [
     'DiamondLoupeFacet',
@@ -25,40 +25,45 @@ const FacetCutAction = {
     Remove: 2
 };
 
+// Helper function to get function selectors using ethers
 function getSelectors(contract) {
-    const signatures = contract.abi
-        .filter(a => a.type === 'function')
+    const selectors = contract.abi
+        .filter(item => item.type === 'function')
         .map(f => {
             if (f.name !== 'init(bytes)') {
-                const functionSignature = f.name + '(' + f.inputs.map(input => input.type).join(',') + ')';
-                const selector = tronWeb.utils.crypto.sha3(functionSignature).substring(2, 10);
-                return '0x' + selector;
+                const functionSignature = ethers.utils.Fragment.from(f).format();
+                return ethers.utils.id(functionSignature).slice(0, 10);
             }
             return null;
         })
         .filter(selector => selector !== null);
-    return signatures;
+    return selectors;
 }
 
-function generateSelector(functionSignature) {
-    return '0x' + crypto.createHash('sha256')
-        .update(functionSignature)
-        .digest('hex')
-        .slice(0, 8);
+// Helper function to encode initialization call data using ethers
+function encodeInitializerFunction(diamondInit, functionName, args = []) {
+    const iface = new ethers.utils.Interface(diamondInit.abi);
+    return iface.encodeFunctionData(functionName, args);
 }
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
     console.log(`Deploying to network: ${network}`);
     console.log('Using account:', accounts);
     const diamondOwner = accounts;
-    
+
+    // Initialize TronWeb (you'll need to add your own configuration)
+    const tronWeb = new TronWeb({
+        fullHost: 'https://nile.trongrid.io/', // Replace with your network
+        privateKey: process.env.PRIVATE_KEY_NILE // Make sure to set this in your environment
+    });
+
     try {
-        // Deploy facets
+        // Deploy DiamondCutFacet
         await deployer.deploy(DiamondCutFacet);
         const diamondCutFacet = await DiamondCutFacet.deployed();
         console.log('DiamondCutFacet deployed:', diamondCutFacet.address);
 
-        // Deploy Diamond with diamondCut
+        // Deploy Diamond
         await deployer.deploy(Diamond, diamondOwner, diamondCutFacet.address);
         const diamond = await Diamond.deployed();
         console.log('Diamond deployed:', diamond.address);
@@ -81,7 +86,7 @@ module.exports = async function(deployer, network, accounts) {
             facets[FacetName] = facet.address;
 
             const selectors = getSelectors(facet);
-            console.log('Selectors for', FacetName, ':', selectors); // Debug log
+            console.log('Selectors for', FacetName, ':', selectors);
 
             cut.push({
                 facetAddress: facet.address,
@@ -89,32 +94,42 @@ module.exports = async function(deployer, network, accounts) {
                 functionSelectors: selectors
             });
         }
+        // Encode initialization call
+        const initData = encodeInitializerFunction(diamondInit, 'init');
 
-        console.log('Diamond Cut:', cut);
+        // Convert the diamond cut data for TronWeb
+        const diamondCutData = cut.map(facetCut => ({
+            facetAddress: TronWeb.address.toHex(facetCut.facetAddress),
+            action: facetCut.action,
+            functionSelectors: facetCut.functionSelectors.map(selector =>
+                selector.startsWith('0x') ? selector : '0x' + selector
+            )
+        }));
 
-        // Create contract instance using TronBox artifacts
-        const diamondCutContract = await DiamondCutFacet.at(diamond.address);
-
-        // Encode init function call
-        const initFunctionSignature = 'init';
-        const functionCall = generateSelector(initFunctionSignature);
-
-        // Perform diamond cut using TronBox contract instance
+        // Perform diamond cut using TronWeb
         console.log('Performing diamond cut...');
-        const tx = await diamondCutContract.diamondCut(
-            cut,
-            diamondInit.address,
-            functionCall,
-            { from: diamondOwner }
+        const txParams = {
+            facetCuts: diamondCutData,
+            init: diamondInit.address,
+            _calldata: initData
+        };
+
+        const transaction = await tronWeb.transactionBuilder.triggerSmartContract(
+            diamond.address,
+            'diamondCut(tuple(address,uint8,bytes4[])[],address,bytes)',
+            {},
+            txParams,
+            diamondOwner
         );
 
-        console.log('Diamond cut tx:', tx.txID); // Note: txID instead of txid
+        const signedTx = await tronWeb.trx.sign(transaction);
+        const receipt = await tronWeb.trx.sendRawTransaction(signedTx);
 
-        // Wait for transaction confirmation using TronWeb
-        await tronWeb.Trx.getTransaction(tx.txID); // Wait for transaction to be confirmed
-        console.log('Transaction confirmed');
+        console.log('Diamond cut transaction:', receipt.txid);
 
-        console.log('Completed diamond cut');
+        // Wait for transaction confirmation
+        await tronWeb.trx.getTransactionInfo(receipt.txid);
+        console.log('Diamond cut confirmed');
 
         const deploymentInfo = {
             diamond: diamond.address,
@@ -124,9 +139,8 @@ module.exports = async function(deployer, network, accounts) {
         };
 
         console.log('Deployment Info:', deploymentInfo);
-
         return diamond.address;
-        
+
     } catch (error) {
         console.error('Deployment failed:', error);
         throw error;
